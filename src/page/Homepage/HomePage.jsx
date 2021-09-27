96a136cd097348e00899ec4f5a14234d905a2248
Fahad Nadeem <mfahad017@gmail.com>
2021-09-27 16:53:40 +0500

    top review done

--- src/page/Homepage/HomePage.jsx
+++ src/page/Homepage/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Contact from './components/contact/Contact'
+import TopReview from './components/topreview/TopReview'
 
 function HomePage() {
     return (
         <div className="homepage">
+            <div className="homepage__topReview-container">
+                <TopReview />
+            </div>
             <div className="homepage__contact-container">
                 <Contact />
             </div>

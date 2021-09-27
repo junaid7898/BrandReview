e84b42f3ce4b70c4748e8665b52e57a29703ef8a
Fahad Nadeem <mfahad017@gmail.com>
2021-09-27 17:33:12 +0500

    home page header done

--- src/page/Homepage/HomePage.jsx
+++ src/page/Homepage/HomePage.jsx
@@ -4,11 +4,15 @@ import Contact from './components/contact/Contact'
 import TopReview from './components/topreview/TopReview'
 
 import TopTenBrands from './components/top-ten-brands/TopTenBrands'
+import Header from './components/Header/Header'
 
 
 function HomePage() {
     return (
         <div className="homepage">
+            <div className="homepage__header-container">
+                <Header />
+            </div>
             <div className="homepage__topReview-container">
                 <TopReview />
             </div>

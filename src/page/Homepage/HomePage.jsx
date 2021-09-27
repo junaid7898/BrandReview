4d9265406390b27290b44a52aab8fd1fb4f6e0f3
junaidNaqeer <89851526+junaidNaqeer@users.noreply.github.com>
2021-09-27 16:52:57 +0500

    top brands done

--- src/page/Homepage/HomePage.jsx
+++ src/page/Homepage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Contact from './components/contact/Contact'
+import TopTenBrands from './components/top-ten-brands/TopTenBrands'
 
 function HomePage() {
     return (
         <div className="homepage">
             <div className="homepage__contact-container">
-                <Contact />
+                <TopTenBrands/>   
+                {/* <Contact /> */}
             </div>
         </div>
     )

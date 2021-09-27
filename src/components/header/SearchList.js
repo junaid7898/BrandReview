c994fe87322837f1a2fb24dc46bdefa2a1a53c51
junaidNaqeer <89851526+junaidNaqeer@users.noreply.github.com>
2021-09-27 12:29:37 +0500

    admin page half done

--- src/components/header/SearchList.js
+++ src/components/header/SearchList.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import CarImage from '../../assests/images/car_image.jpg'
 
-const SearchList = ({styling, showResult, data}) => {
+const SearchList = ({styling ,divStyling,  data, setShowResult}) => {
+    
+    
     return (
-        <div className = {styling} style = {{display: showResult}}>
-            {data.map((item) => {
+        <div className = {styling}>
+            {
+                data ?
+                data.map((item) => {
                 return(
-                    <h1>{item}</h1>
+                    <Link to ={`${"/"}`} className = {divStyling} onClick = { () => setShowResult(false)}>
+                        <img src = {item.img} style = {{width: 50, borderRadius: 2  }}/>
+                        <h1>{item.name}</h1>
+                    </Link>
+                    
                 )
-            })}
+            }) :
+                <h3 style={{padding: 10}}>
+                    No Relavant Resutls
+                </h3>
+            }
         </div>
     )
 }

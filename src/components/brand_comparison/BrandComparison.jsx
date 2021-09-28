df4e128d247db4633f3429b29ac7cf8f012eca7b
junaidNaqeer <89851526+junaidNaqeer@users.noreply.github.com>
2021-09-28 13:17:10 +0500

    inital push

---
+++ src/components/brand_comparison/BrandComparison.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BrandComparison = () => {
+    return (
+        <div>
+            <h1>brand comparision</h1>
+        </div>
+    )
+}
+
+export default BrandComparison

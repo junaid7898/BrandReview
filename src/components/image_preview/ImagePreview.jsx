c613568c23a4bbbc5d0a86fb3c698d5618b30dde
Fahad Nadeem <41478726+mfahad017@users.noreply.github.com>
2021-10-06 13:20:22 +0500

    image preview complete

---
+++ src/components/image_preview/ImagePreview.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { GrClose } from "react-icons/gr";
+function ImagePreview({image, removeImage}) {
+    return (
+        <div className="imagePreview">
+            <GrClose className="imagePreview__close"/>
+            <img className="imagePreview__image" src="https://i2.wp.com/blog.viral-launch.com/wp-content/uploads/2018/08/2000px-Apple_logo_black.svg_.png?fit=2000%2C2000&ssl=1" alt="logo" />
+        </div>
+    )
+}
+
+export default ImagePreview

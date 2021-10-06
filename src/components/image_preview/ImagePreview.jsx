d436a7626bfd5b36903406c5f6b478944b2c850f
Fahad Nadeem <41478726+mfahad017@users.noreply.github.com>
2021-10-06 13:46:51 +0500

    imgae preview delete

--- src/components/image_preview/ImagePreview.jsx
+++ src/components/image_preview/ImagePreview.jsx
@@ -3,7 +3,7 @@ import { GrClose } from "react-icons/gr";
 function ImagePreview({image, removeImage}) {
     return (
         <div className="imagePreview">
-            <GrClose className="imagePreview__close"/>
+            <GrClose className="imagePreview__close" size={24}/>
             <img className="imagePreview__image" src="https://i2.wp.com/blog.viral-launch.com/wp-content/uploads/2018/08/2000px-Apple_logo_black.svg_.png?fit=2000%2C2000&ssl=1" alt="logo" />
         </div>
     )

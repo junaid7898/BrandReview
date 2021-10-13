0aca1acfa1cdb5af5da05fcf78afbea2d3dab82f
Fahad Nadeem <41478726+mfahad017@users.noreply.github.com>
2021-10-13 13:36:07 +0500

    latest code

--- src/page/profile/components/ProfileDetail/components/MyDetails/MyDetails.jsx
+++ src/page/profile/components/ProfileDetail/components/MyDetails/MyDetails.jsx
@@ -10,34 +10,11 @@ const MyDetails = ({ user }) => {
   const [countryCode, setCountryCode] = useState(null);
   const [email, setEmail] = useState(null);
 
-<<<<<<< HEAD
+
   const [updateProfile, setUpdateProfile] = useState(false);
   const onSubmit = () => {
     console.log("data is uploaded");
   };
-=======
-    return (
-        <section className = 'mydetails'>
-                
-                {
-                    user ?
-                        <>
-                            <DetailTag class1 = 'tag ' label = 'phone' value = {user.phoneNumber}/>
-                            <DetailTag class1 = 'tag ' label = 'Birthday' value = {user.dateOfBirth}/>
-                            <DetailTag class1 = 'tag tag__address' label = 'address' value = {user.address} />
-                            <DetailTag class1 = 'tag ' label = 'Country Code' value = {user.countryCode}/>
-                            <DetailTag class1 = 'tag ' label = 'Email' value = {user.email}/>
-                        </>
-                    :
-                        null
-                }
-                
-        </section>
-    )
-}
->>>>>>> main
-
-  
 
   return (
     <section className="mydetails">

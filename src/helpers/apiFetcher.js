d8f6761f4eaa33c8171ae7f331c40a417ea42463
Fahad Nadeem <41478726+mfahad017@users.noreply.github.com>
2021-10-30 18:51:19 +0500

    backend url changed

--- src/helpers/apiFetcher.js
+++ src/helpers/apiFetcher.js
@@ -2,7 +2,7 @@
 
 export const fetcher = (endpoint, token, clientType, client, dispatch, payload ) => {
 
-    const baseUrl = "http://localhost:4000/v1"
+    // const baseUrl = "http://localhost:4000/v1"
 
     if(clientType === "user"){
 
